67f98d8976d9cd5457d37f0c55f747a48f0b2630
Sirawdink Abayneh <sirawdinkabayneh77@gmail.com>
2026-03-22 12:35:31 +0300

    Initial backend setup

---
+++ backend/app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_cors import CORS
+
+def create_app():
+   app = Flask(__name__)
+
+   CORS(app)
+
+   @app.route('/')
+   def home():
+      return {"message": "Sirawdink Backend is running "}
+   
+   return app
